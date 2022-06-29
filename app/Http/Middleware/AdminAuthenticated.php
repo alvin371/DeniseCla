779cc0c67433157c02ccca50aea3571bd419bd25
alvin371 <alvindimas8@gmail.com>
2022-06-29 12:50:54 +0700

    update on admin auth

--- app/Http/Middleware/AdminAuthenticated.php
+++ app/Http/Middleware/AdminAuthenticated.php
@@ -30,7 +30,7 @@ class AdminAuthenticated
             }
 
             // allow admin to proceed with request
-            else if ($user->hasRole('admin')) {
+            else if ($user->role == 'admin') {
                 return $next($request);
             }
         }
